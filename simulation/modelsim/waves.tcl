47508d1f955c6d736c8ea5da67bb26fbe04bddd8
SyncAdam <61192656+SyncAdam@users.noreply.github.com>
2026-03-14 14:25:28 +0100

    Cleanup binaries
    * Delete db directory
    
    * Delete incremental_db directory
    
    * Delete output_files directory
    
    * Delete simulation directory
    
    * Update .gitignore

--- simulation/modelsim/waves.tcl
+++
@@ -1,8 +0,0 @@
-onerror {resume}
-
-add wave -noupdate -label clk /TestBench/clock
-add wave -noupdate -label Output /TestBench/outputValue
-
-TreeUpdate [SetDefaultTree]
-WaveRestoreCursors {{Cursor 1} {10 ns} 0}
-update
